207a3b16d593d5d8634717e70036e5e102181d0b
sameer-b <bs.sameer1@gmail.com>
2012-11-11 00:57:59 +0530

    updated UI

--- animations.js
+++ animations.js
@@ -1,8 +1,3 @@
-
-
-
-
-
 function getInfo()
 {
 if(localStorage.loc1==undefined)
@@ -57,14 +52,11 @@ document.getElementById("result9").innerHTML=" " + localStorage.loc9;
 document.getElementById("result10").innerHTML="  " + localStorage.loc10;
 }
 
-
-
-
 $(document).ready(function(){
   $("#showSettings").click(function(){
     $("#wrap").hide(1000);
     $("#settings").show(1000);
-    getInfo();
+     getInfo();
   });
 });
 
@@ -72,6 +64,7 @@ $(document).ready(function(){
   $("#back").click(function(){
     $("#settings").hide(1000);
     $("#wrap").show(1000);
-    getInfo();
+     getInfo();
   });
 });
+
